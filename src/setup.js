ae3a776aca275e27fdde11b2a339283078d067dc
Victor Warno <victor.warno@senacor.com>
2020-01-02 23:35:01 +0100

    Added gravity to a box element, added a camera, added laser controls, refactored getting and setting positions

--- src/setup.js
+++ src/setup.js
@@ -2,28 +2,39 @@ AFRAME.registerComponent('setup', {
 
     init: function () {
         const sceneEl = document.querySelector('a-scene');
+        const colors = ['#EF2D5E', '#4CC3D9', '#FFC65D', '#34B51D'];
 
         for (j = 0; j < 5; j++) {
             for (i = 0; i < 9; i++) {
                 let boxEl = document.createElement('a-box');
                 boxEl.setAttribute('id', 'box' + i + j);
-                boxEl.setAttribute('material', {color: '#EF2D5E'});
-                boxEl.setAttribute('position', {x: (i - 4) * 1.2, y: (j + 1) * 1.2, z: -5});
+                boxEl.setAttribute('material', {color: colors[0]});
+                boxEl.object3D.position.set((i - 4) * 1.2, (j + 1) * 1.2, -5);
+
                 sceneEl.appendChild(boxEl);
             }
         }
 
-        let thatBox = document.querySelector('#box42');
-        const xThatBox = thatBox.getAttribute('position').x;
-        const yThatBox = thatBox.getAttribute('position').y;
-        const zThatBox = thatBox.getAttribute('position').z;
-        thatBox.setAttribute('material', {color: '#4CC3D9'});
-        thatBox.setAttribute('animation', `property: position; to: -1.2 3.6 -5; dir: alternate; loop: true`);
+        let thatBoxEl = document.querySelector('#box42');
+        let thatPosition = thatBoxEl.object3D.position;
 
-        let thisBox = document.querySelector('#box32');
-        thisBox.setAttribute('material', {color: '#FFC65D'});
-        thisBox.setAttribute('animation', `property: position; to: ${xThatBox} 3.6 -5; dir: alternate; loop: true`);
-        console.log(thisBox.getAttribute('position').x);
+        let thisBoxEl = document.querySelector('#box32');
+        let thisPosition = thisBoxEl.object3D.position;
+
+        thatBoxEl.setAttribute('material', {color: '#4CC3D9'});
+        thatBoxEl.setAttribute('animation', `property: position; to: ${thisPosition.x} ${thisPosition.y} ${thisPosition.z}; dir: alternate; loop: true`);
+        thisBoxEl.setAttribute('material', {color: '#FFC65D'});
+        thisBoxEl.setAttribute('animation', `property: position; to: ${thatPosition.x} ${thatPosition.y} ${thatPosition.z}; dir: alternate; loop: true`);
+
+        let firstBoxEl = document.querySelector('#box44');
+        firstBoxEl.setAttribute('material', {color: colors[3]});
+        firstBoxEl.setAttribute('dynamic-body', {
+            shape: 'box',
+            mass: 1,
+            linearDamping: 0.005
+        });
+        firstBoxEl.setAttribute('event-set__enter', {_event: 'mouseenter', color: '#8FF7FF'});
+        firstBoxEl.setAttribute('event-set__leave', {_event: 'mouseleave', color: '#34B51D'});
     }
 
 });
