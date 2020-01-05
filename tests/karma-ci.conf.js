709686173cdcfd631462cc3cd612f8e17415046a
Victor Warno <victor.warno@senacor.com>
2020-01-05 17:38:41 +0100

    Added new karma-ci configuration for CI with headless Firefox and Chrome, github workflow is added, package-lock.json is added

---
+++ tests/karma-ci.conf.js
@@ -0,0 +1,32 @@
+// Karma configuration.
+module.exports = function (config) {
+  config.set({
+    basePath: '../',
+    browserify: {
+      debug: true,
+      paths: ['./']
+    },
+    browsers: ['FirefoxHeadless', 'ChromeHeadless'],
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: [ '-headless' ],
+      }
+    },
+    client: {
+      captureConsole: true,
+      mocha: {ui: 'tdd'}
+    },
+    envPreprocessor: ['TEST_ENV'],
+    files: [
+      // Define test files.
+      {pattern: 'tests/**/*.test.js'},
+      // Serve test assets.
+      {pattern: 'tests/assets/**/*', included: false, served: true}
+    ],
+    frameworks: ['mocha', 'sinon-chai', 'chai-shallow-deep-equal', 'browserify'],
+    preprocessors: {'tests/**/*.js': ['browserify', 'env']},
+    reporters: ['mocha'],
+    singleRun: true
+  });
+};
