6d0a855a69c3d5fe73be2ee45a65d25ea5d3eeff
Victor Warno <35118557+Schmowser@users.noreply.github.com>
2020-02-01 12:53:21 +0100

    Generalize grid and quick fix for gravity (#20)
    * Generalized the grid to take width and height for random generation of cubes mixins
    * Reconfigured example grid for debugging gravity, quickfix for gravity: Pause before execution of gravity as not all ids are updated yet, added FIXME
    * Disable test suite for quickfix

--- src/grid.js
+++ src/grid.js
@@ -1,41 +1,57 @@
 AFRAME.registerComponent('grid', {
+    schema: {
+        width: {type: 'number', default: 6},
+        height: {type: 'number', default: 12}
+    },
 
     init: function () {
-        // TODO: Add code that initializes the child cubes inside the grid!
-
+        const data = this.data;
         const el = this.el;
-        el.addEventListener('checkForMatch', function () {
+        const width = data.width;
+        const height = data.height;
 
-            // Check for all matches in each row
-            const rowNodesToBeRemoved = getMatchNodes(true);
+        // EVENT LISTENERS
+        el.addEventListener('checkForMatch', function () { // FIXME: Gets called 4 times (each time animation ends)
 
-            // Check for all matches in each column
-            const colNodesToBeRemoved = getMatchNodes(false);
+            const rowNodesToBeRemoved = getMatchNodes(true, width, height);
+            const colNodesToBeRemoved = getMatchNodes(false, width, height);
 
-            // Remove all nodes
+            // TODO: Query for all invisibles and add them to nodeToBeRemoved
             let nodesToBeRemoved = new Set([...rowNodesToBeRemoved, ...colNodesToBeRemoved]);
             nodesToBeRemoved.forEach(node => node.parentNode.removeChild(node));
-            // nodesToBeRemoved.forEach(node => node.setAttribute('visible', false));
 
             applyGravityAfterBlockRemoval(nodesToBeRemoved);
 
+            // TODO: Fill grid with invisibles
         });
 
-        el.emit('checkForMatch');
+        // INITIALIZE
+        initGrid(el, width, height);
+
+        // EMIT EVENTS
+        setTimeout(() => {
+            el.emit('checkForMatch');
+        }, 200); // We have to wait a bit until all elements are flushed to the DOM, maybe use flushToDOM somewhere?
     },
 
 });
 
-function getMatchNodes(isRow) {
+function getMatchNodes(isRow, width, height) {
     let matchNodes = new Set();
     let tempNodes = new Set();
     let currentColor = null;
-    for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) { // TODO: Generalize 5
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
             let id = isRow ? buildId(i, j) : buildId(j, i);
             const node = document.getElementById(id);
-            if (node != null) {
-                const nodeColor = node.getAttribute('material').color;
+            if (node) {
+                const material = node.getAttribute('material');
+                let nodeColor = null;
+                if (material) {
+                    nodeColor = material.color;
+                } else {
+                    continue // TODO: Check logic when a node does not have a color
+                }
 
                 if (node.getAttribute('visible') && nodeColor === currentColor) {
                     tempNodes.add(node);
@@ -57,6 +73,23 @@ function getMatchNodes(isRow) {
     return matchNodes;
 }
 
+function initGrid(grid, width, height) {
+    const colors = ['#EF2D5E', '#4CC3D9', '#FFC65D', '#34B51D'];
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            let cubeEl = document.createElement('a-entity');
+            cubeEl.setAttribute('mixin', 'cube');
+            let id = buildId(i, j);
+            cubeEl.setAttribute('id', id);
+            cubeEl.setAttribute('material', {color: colors[getRandomInt(colors.length)]});
+            let position = idToPositionMapper(id);
+            cubeEl.object3D.position.set(position.x, position.y, position.z);
+            grid.appendChild(cubeEl);
+        }
+    }
+}
+
 function buildId(i, j) {
     const iString = '' + i;
     const jString = '' + j;
@@ -65,24 +98,26 @@ function buildId(i, j) {
 
 function applyGravityAfterBlockRemoval(nodesRemoved) {
     let cubeNodes = document.querySelectorAll("[mixin='cube']");
-    cubeNodes.forEach(block => {
-        const row = block.id[0];
-        const col = block.id[1];
-        let drop = [...nodesRemoved]
-            .filter(node => node.id[1] === col)
-            .filter(node => node.id[0] < row)
-            .length;
-        if (drop) {
-            const targetPosition = idToPositionMapper(buildId(row - drop, col));
-            const fallDuration = 400 * Math.sqrt(drop);
-            block.setAttribute('animation', `
+    setTimeout(() => {
+        cubeNodes.forEach(block => {
+            const row = block.id[0];
+            const col = block.id[1];
+            let drop = [...nodesRemoved]
+                .filter(node => node.id[1] === col)
+                .filter(node => node.id[0] < row)
+                .length;
+            if (drop) {
+                const targetPosition = idToPositionMapper(buildId(row - drop, col));
+                const fallDuration = 800 * Math.sqrt(drop);
+                block.setAttribute('animation', `
                         property: position;
                         to: ${targetPosition.x} ${targetPosition.y} ${targetPosition.z};
                         easing: easeInSine;
                         dur: ${fallDuration};
                     `);
-        }
-    });
+            }
+        });
+    }, 100); // FIXME: Remove if calling checkForMatch 4-times is solved
 }
 
 function idToPositionMapper(id) {
@@ -92,4 +127,8 @@ function idToPositionMapper(id) {
     position.y = id[0] * interSpace;
     position.z = 0;
     return position
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
